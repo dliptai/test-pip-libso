4ffac2c64b5f1bde36c5dbae0d8a2296b260bb38
David Liptai <dliptai@swin.edu.au>
2020-07-13 17:47:40 +1000

    renamed scripts

--- build-wheels.sh
+++ build-wheels.sh
@@ -6,6 +6,6 @@ for V in 1 2010 2014; do
   for ARCH in i686 x86_64; do
     PLAT="manylinux${V}_${ARCH}"
     DOCKER_IMAGE="quay.io/pypa/${PLAT}"
-    docker run --rm -e PLAT=$PLAT -e ARCH=$ARCH -v `pwd`:/io $DOCKER_IMAGE /io/builder.sh
+    docker run --rm -e PLAT=$PLAT -e ARCH=$ARCH -v `pwd`:/io $DOCKER_IMAGE /io/docker/builder_manylinux.sh
   done
 done
