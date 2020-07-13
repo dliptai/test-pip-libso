7576ca4e21af01a1c673c1eccd67d3e3f2cfd172
David Liptai <dliptai@swin.edu.au>
2020-07-13 14:26:38 +1000

    dont need to copy .so for each python version, only once for each platform

--- builder.sh
+++ builder.sh
@@ -9,14 +9,13 @@ fi
 
 cd /io/pydave
 
+# Make copy of libgfortran.so to include in wheel
+cp /usr/lib${X}/libgfortran.so.? ./pydave/libs/.
+
 # Compile wheels
 for PYBIN in /opt/python/cp3*/bin; do
-
-  # Make copy of libgfortran.so to include in wheel
-  cp /usr/lib${X}/libgfortran.so.? ./pydave/libs/.
-
   "${PYBIN}/python" setup.py bdist_wheel --plat $PLAT
-
-  # Clean libs folder
-  rm ./pydave/libs/*.so*
 done
+
+# Clean libs folder
+rm ./pydave/libs/*.so*
