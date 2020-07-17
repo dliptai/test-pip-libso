a35a04784c53046ebd7216654b50f64db4bd7b15
David Liptai <dliptai@swin.edu.au>
2020-07-17 12:57:51 +1000

    compile fortran when bdist_wheel command run() is called from setup(), instead of always when setup.py script is run.

--- pydave/setup.py
+++ pydave/setup.py
@@ -1,14 +1,7 @@
 import subprocess
 import setuptools
 from setuptools.dist import Distribution
-from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
-
-"""
-Build the fortran library
-"""
-subprocess.call(['make', 'clean'], cwd='../')
-subprocess.call(['make', 'lib'], cwd='../')
-subprocess.call(['cp', '../bin/libdave.so', './src/libs/.'])
+from wheel.bdist_wheel import bdist_wheel
 
 """
 Declare binary wheels as 'non-pure'.
@@ -21,25 +14,32 @@ The package does not depend on any specific version of CPython.
 Thus we remove the abi tag, and set the python tag to the major
 version of python being used (i.e. py3)
 """
-class bdist_wheel(_bdist_wheel):
+class BinaryDistribution(Distribution):
+    def is_pure(self):
+        return False
 
-    def finalize_options(self):
-        _bdist_wheel.finalize_options(self)
-        # Mark us as not a pure python package
-        self.root_is_pure = False
-
+class custom_bdist_wheel(bdist_wheel):
     def get_tag(self):
-        python, abi, plat = _bdist_wheel.get_tag(self)
+        python, abi, plat = bdist_wheel.get_tag(self)
         # We don't use CPython
-        python, abi = self.python_tag, 'none'
+        python, abi, plat = self.python_tag, 'none', self.plat_name
         return python, abi, plat
+
+    def run(self):
+        """
+        Build the fortran library first, then do regular run()
+        """
+        subprocess.call(['make', 'clean'], cwd='../')
+        subprocess.call(['make', 'lib'], cwd='../')
+        subprocess.call(['cp', '../bin/libdave.so', './src/libs/.'])
+        bdist_wheel.run(self)
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name             = "pydave",
-    version          = "0.0.7   ",
+    version          = "0.0.7",
     author           = "David Liptai",
     author_email     = "dliptai@swin.edu.au",
     description      = "A python wrapper for the 'dave' library",
@@ -48,8 +48,10 @@ setuptools.setup(
     # url="https://github.com/pypa/sampleproject",
     packages     = ["pydave"],
     package_dir  = {"pydave" : "src"},
-    package_data = {"pydave" : ["libs/*.so*"]},
+    package_data={"pydave": ["libs/*.so*", "libs/*.dylib*"]},
+    include_package_data = True,
     classifiers  = ["Programming Language :: Python :: 3",],
     python_requires='>=3.6',
-    cmdclass={'bdist_wheel': bdist_wheel},
+    cmdclass={'bdist_wheel': custom_bdist_wheel},
+    distclass=BinaryDistribution
 )
