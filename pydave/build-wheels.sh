2a198617c3553a19b0c9b7d40e875a79ea2fd747
David Liptai <dliptai@swin.edu.au>
2020-08-11 17:55:51 +1000

    check that delocate/auditwheel is installed, and set AUDITWHEEL_PLAT for standard linux images

--- pydave/build-wheels.sh
+++ pydave/build-wheels.sh
@@ -1,12 +1,11 @@
-#!/bin/bash
-set -e -x
+#!/bin/bash -ex
 
-# If MANLINUX is not set, assume no
+# If MANYLINUX is not set, assume no
 if [ -z "$MANYLINUX" ]; then
   MANYLINUX=no
 fi
 
-#-- Script requires environment variable PLAT to be set
+#-- Make sure variables are not used uninitialised
 set -u
 
 # Use manylinux pythons if MANYLINUX=yes
@@ -18,20 +17,42 @@ else
 fi
 
 OS=$(uname -s)
+ARCH=$(uname -m)
 
 # Choose between delocate and auditwheel, and set the directory
 # (within the package) to copy the external libraries to.
-# Also, hardcode the platform tag for MacOS, otherwise deallocate won't work.
-# Note that auditwheel will automatically change the platform tag.
+# Also, hardcode the platform tag for MacOS, otherwise delocate won't work.
 if [ "${OS}" == "Darwin" ]; then
-  DELOCATE_TOOL='delocate-wheel -v'
+  DELOCATE_TOOL='delocate-wheel'
+  DELOCATE_CMD='delocate-wheel -v'
   LIB_DIR=libs/.dylibs
   PLAT=macosx_10_9_x86_64
   PLAT_FLAG="--plat-name ${PLAT}"
 else
-  DELOCATE_TOOL='auditwheel repair'
+  DELOCATE_TOOL='auditwheel'
+  DELOCATE_CMD='auditwheel repair'
   LIB_DIR=/libs/
   PLAT_FLAG=
+
+  # Note that auditwheel will automatically change the platform tag based on
+  # the $AUDITWHEEL_PLAT environment variable (which is defined on manylinux images).
+
+  if [ "${MANYLINUX}" == "yes" ]; then
+    # Check that AUDITWHEEL is set
+    if [ -z "$AUDITWHEEL_PLAT" ]; then
+      echo "This doesn't look like a manylinux image. AUDITWHEEL_PLAT is not set!"
+      exit 1
+    fi
+  else
+    # Set AUDITWHEEL_PLAT for standard linux
+    export AUDITWHEEL_PLAT="linux_${ARCH}"
+  fi
+
+fi
+
+if ! hash ${DELOCATE_TOOL} >/dev/null 2>&1; then
+  echo "You need ${DELOCATE_TOOL} installed to use this script"
+  exit 1
 fi
 
 # Set directories for bad and good wheels
@@ -53,7 +74,7 @@ done
 # Delocate wheels (remove external lib dependencies by including relevant libs in wheel)
 # Note: these tools also relink libraries for you by modifying their ELFs
 for whl in ${BAD_WHEELS}/*.whl; do
-  ${DELOCATE_TOOL} -L ${LIB_DIR} -w ${WHEELHOUSE} ${whl}
+  ${DELOCATE_CMD} -L ${LIB_DIR} -w ${WHEELHOUSE} ${whl}
 done
 
 # Delete the bad wheels folder
