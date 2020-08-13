d430739faf3ea14feacbe3aaacb12ce62b44fd59
David Liptai <dliptai@swin.edu.au>
2020-08-13 17:50:58 +1000

    test

--- pydave/twine-upload.sh
+++ pydave/twine-upload.sh
@@ -1,3 +1,9 @@
+#!/bin/bash -eu
+
+if ! hash twine >/dev/null 2>&1; then
+  echo "You need twine installed to use this script"
+  exit 1
+fi
 
 twine upload              \
   --skip-existing         \
